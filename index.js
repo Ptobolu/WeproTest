0842abea247151f899695c66373ea162d164e188
petrotobolu <s11610047@student.unklab.ac.id>
2020-11-03 08:06:22 -0800

    first

---
+++ index.js
@@ -0,0 +1,83 @@
+// calculateAge = (birthYear) => {
+// 	return 2019 - birthYear;
+// }
+ 
+// yearUntilRetirement = (object) =>{
+// 	var age = calculateAge = (object.year);
+// 	var retirement = 60 - age;
+   
+// 	if(retirement > 0){
+//     	console.log(object.firstName + ' retires in '+ retirement + ' years');
+// 	} else {
+//     	console.log(object.firstName + ' is already retired.');
+// 	}
+// }
+ 
+// yearUntilRetirement({year: 1987, firstName: 'John'});
+ 
+
+
+// const addNumber = new Map()
+//     addNumber.set('a,','1')
+//     addNumber.set('b,','2')
+//     addNumber.set('c','3')
+//     addNumber.set('d','4')
+//     addNumber.set('e','5')
+//     addNumber.set('f','6')
+//     addNumber.set('g','7')
+
+//     const sum = addNumber.values()
+//     console.log(sum)
+
+//  addNumber = new Map([ 
+//  ["a",1],
+//  ["b",2],
+//  ["c",3],
+//  ["d",4],
+//  ["e",5],
+//  ["f",6],
+//  ["g",7],
+// ]);
+
+// function calculateAge(Array){
+// var phi = 3.14;
+// var power = 2;
+// var radius = 0;
+
+// var result = Array.map(function(obj) {
+//   return phi * Math.pow(obj.radius, obj.power);
+// });
+ 
+// radius = 21;
+// var area21 = calculateArea({radius: radius, power: 2 });
+ 
+// radius = 7;
+// var area7 = calculateArea({radius: radius, power: 2 });
+ 
+// console.log('area with 21 radius: ' + area21 + ', and area with 7 radius: ' + area7);
+// }
+
+// const makeAjaxRequest = (url, method) => {
+//   if(!method){
+//        method = ‘GET’
+//   }
+//   console.log(url, method);
+//   }
+   
+//   makeAjaxRequest(‘www.google.com’);
+  
+
+// function warnaKesukaan() {
+//   return "Putih";
+// }
+
+// let warna = `Warna Kesukaan saya adalah ${warnaKesukaan()}`;
+
+// console.log(warna);
+
+// let buah1 = ['apel', 'mangga'];
+// let buah2 = ['sirsak', 'jeruk'];
+
+// let semuaNamaBuah = [...buah1, ...buah2];
+// console.log(semuaNamaBuah);
+
